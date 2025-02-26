3bcc45e74e3637d8f01fd96a5632bb78c652cec4
Edward Hartnett <edward.hartnett@noaa.gov>
2025-02-25 18:36:35 -0700

    GRIBbot added

---
+++ GRIBbot.py
@@ -0,0 +1,11 @@
+class GRIBbot:
+    def __init__(self):
+        print("GRIBbot initialized")
+
+    def main(self):
+        print("GRIBbot main")
+
+if __name__=="__main__":
+    gribbot = GRIBbot()
+    gribbot.main()
+    
