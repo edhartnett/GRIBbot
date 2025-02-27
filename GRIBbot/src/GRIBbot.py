f72fa8aadb09765f86bb170895f7144350780a38
Ed <edward.hartnett@noaa.gov>
2025-02-27 12:30:03 -0700

    changes

--- GRIBbot/src/GRIBbot.py
+++ GRIBbot/src/GRIBbot.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
 class GRIBbot:
     def __init__(self):
         print("GRIBbot initialized")
@@ -9,3 +12,22 @@ if __name__=="__main__":
     gribbot = GRIBbot()
     gribbot.main()
     
+load_dotenv(find_dotenv())
+print(os.getenv("OPENAI_API_KEY"))
+
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
